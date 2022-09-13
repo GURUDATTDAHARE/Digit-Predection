40cac872fb2e0b633c13d232f94bc9c7d077140d
KunalAnkur <mrankur801@gmail.com>
2022-09-14 02:01:14 +0530

    developing ui

--- static/script.js
+++ static/script.js
@@ -1,7 +1,4 @@
-
 var canvas = document.getElementById("draw");
-var but = document.getElementById("predict")
-var input=document.getElementById("inp")
 var ctx = canvas.getContext("2d");
 
 resize();
@@ -9,8 +6,6 @@ function resize() {
   ctx.canvas.width = window.innerWidth;
   ctx.canvas.height = window.innerHeight;
 }
-
-
 
 // last known position
 var pos = { x: 0, y: 0 };
@@ -21,36 +16,42 @@ function setPosition(e) {
   pos.y = e.clientY;
 }
 
+async function onSubmit() {
+  var dataURL = canvas.toDataURL();
+  try {
+    const res = await fetch("/predict", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ imageURL: dataURL }),
+    });
+    const content = await res.json();
+    console.log(content);
+  } catch (error) {
+    console.log("Fetch error: ", error);
+  }
+}
 
 function draw(e) {
-    if (e.buttons !== 1) return; // if mouse is not clicked, do not go further
- 
-    var color = document.getElementById("hex").value;
+  if (e.buttons !== 1) return; // if mouse is not clicked, do not go further
 
-    ctx.beginPath(); // begin the drawing path
-    
-    // line properties
-    ctx.lineWidth = 40; // width of line
-    ctx.lineCap = "round"; // rounded end cap
-    ctx.strokeStyle = "#00ffff"; // hex color of line
-    
+  ctx.beginPath(); // begin the drawing path
 
-    // draw line
-    ctx.moveTo(pos.x, pos.y); // from position
-    setPosition(e);
-    ctx.lineTo(pos.x, pos.y); // to position
- 
-    ctx.stroke(); // draw it!
+  // line properties
+  ctx.lineWidth = 40; // width of line
+  ctx.lineCap = "round"; // rounded end cap
+  ctx.strokeStyle = "#00ffff"; // hex color of line
+
+  // draw line
+  ctx.moveTo(pos.x, pos.y); // from position
+  setPosition(e);
+  ctx.lineTo(pos.x, pos.y); // to position
+
+  ctx.stroke(); // draw it!
 }
-but.addEventListener("click",()=>{
-   
-  var dataURL = canvas.toDataURL();
-  // but.value=dataURL
- // console.log(dataURL)
-    // var highQuality = canvas.toDataURL("image/jpeg", 1.0);
-    input.value=""+dataURL
-    //location.href="/predict"
-});
+
 window.addEventListener("resize", resize);
 document.addEventListener("mousemove", draw);
 document.addEventListener("mousedown", setPosition);
